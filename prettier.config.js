493a4a37edef4eeb91864ec89290fb283b8c5b72
DevLabo <monolab000@gmail.com>
2024-01-25 22:20:19 +0900

    eslint-plugin-tailwindcss,prettier-plugin-tailwindcssをインストール

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,7 @@
 const config = {
   semi: true,
   singleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"],
 };
 
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <h1 className="bg-teal-400">Vite + React</h1>
+      <h1 className="text-wrap  font-semibold text-cyan-100">Vite + React</h1>
       <Button>shaduiインストールしたよ！</Button>
     </>
   );
