5074162569adff3aafb9c58fa21074258129287a
DevLabo <monolab000@gmail.com>
2024-02-05 23:51:28 +0900

    DarkMode

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,10 @@
+import { ThemeProvider } from '@/context/ThemeProvider';
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mt-20 flex items-center justify-center">{children}</div>
+    <ThemeProvider>
+      <div className="mt-20 flex items-center justify-center">{children}</div>
+    </ThemeProvider>
   );
 }
 
